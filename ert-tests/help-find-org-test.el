a6a4b7a7ad9420ecbc226e963e994fb3683bef15
Eric Crosson <esc@ericcrosson.com>
2015-03-28 23:05:03 -0500

    attempt again on travis ci

--- ert-tests/help-find-org-test.el
+++ ert-tests/help-find-org-test.el
@@ -1,6 +1,6 @@
 (require 'org)
-(require 'ob)
-(require 'ob-tangle)
+;; (require 'ob)
+;; (require 'ob-tangle)
 
 (defcustom help-find-org-test-lib "help-find-org-test-lib"
   "Test library name for `help-find-org-test'.")
@@ -8,23 +8,21 @@
 (defcustom help-find-org-test-fn 'help-find-org-test-fn
   "Test function provided by `help-find-org-test-lib'.")
 
-;; for easy recursive loading
-(let ((default-directory "."))
-  (normal-top-level-add-to-load-path '("."))
-  (normal-top-level-add-subdirs-to-load-path))
-
 (ert-deftest test-help-find-org ()
   (require (intern help-find-org-test-lib))
   (find-function help-find-org-test-fn)
-  (should (string-equal (file-name-nondirectory (buffer-file-name)) (format "%s.el" help-find-org-test-lib)))
+  (should (string-equal (file-name-nondirectory (buffer-file-name))
+			(format "%s.el" help-find-org-test-lib)))
+
+  ;; reset after the first test
   (kill-buffer)
 
   ;; try again -- this time we should be looking at the org file
   (require 'help-find-org)
   (help-find-org 1)
   (find-function help-find-org-test-fn)
-  (should (string-equal (file-name-nondirectory (buffer-file-name)) (format "%s.org" help-find-org-test-lib)))
-  )
+  (should (string-equal (file-name-nondirectory (buffer-file-name))
+			(format "%s.org" help-find-org-test-lib))))
 
 ;; todo: implement test for variables and libraries
 
